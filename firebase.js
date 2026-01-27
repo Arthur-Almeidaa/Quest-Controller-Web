f251c8f1fc7121751e3424b8bf3512c51c6a910d
Arthur-Almeidaa <arthuralmeidaa66@gmail.com>
2026-01-27 13:56:46 -0300

    first commit

--- firebase.js
+++ firebase.js
@@ -1,8 +1,6 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// 🔥 SUA CONFIG (a mesma que você mandou)
 const firebaseConfig = {
   apiKey: "AIzaSyDAkapYFpjsugAn5lFq8e5pXHdecn75Ej8",
   authDomain: "teste-f579d.firebaseapp.com",
@@ -12,21 +10,16 @@ const firebaseConfig = {
   messagingSenderId: "884652869722",
   appId: "1:884652869722:web:62519ca31c81099e457063"
 };
-""
-// Inicializa Firebase
+
+// 🔹 Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const db = getDatabase(app);
 
-// Caminho no Realtime Database
-const deviceRef = ref(database, "devices/teste1");
+// 🔹 Função pública pra escutar dispositivos
+export function listenDevices(callback) {
+  const devicesRef = ref(db, "devices");
 
-// Escuta em tempo real
-onValue(deviceRef, (snapshot) => {
-  const data = snapshot.val();
-  if (!data) return;
-
-  document.getElementById("battery").innerText = data.battery + "%";
-  document.getElementById("ip").innerText = data.ip;
-  document.getElementById("lastUpdate").innerText =
-    new Date(data.timestamp).toLocaleString();
-});
+  onValue(devicesRef, (snapshot) => {
+    callback(snapshot);
+  });
+}
